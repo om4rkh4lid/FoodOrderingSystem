1c6b4f7b435420aff672ee81b71bfbf436e41342
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-05 19:36:30 +0200

    added the ability to run in development mode

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
+import environment from "dotenv";
 import Express from "express";
+import morgan from "morgan";
 
-const app = Express();
+environment.config();
 
-app.listen(5555, () => {
-  console.log('Listening on port 5555...');
+const app: Express.Application = Express();
+const mode: string = process.env.NODE_ENV!;
+
+if (mode === 'development') {
+  app.use(morgan('dev'));
+}
+
+const host: string = process.env.HOST!
+const port: number = parseInt(process.env.PORT!);
+
+app.listen(port, host, () => {
+  console.log(`Listening on http://${host}:${port} in ${mode} mode...`);
 })
