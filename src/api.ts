15e76e8dc173b6810fc7579286c4fc1ebf1305f9
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-05 20:56:37 +0200

    created GraphQL middleware with a query to get all restaurants

---
+++ src/api.ts
@@ -0,0 +1,40 @@
+import { graphqlHTTP } from "express-graphql"
+import { GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import RestaurantService from "./services/restaurant";
+const apiMiddleware = graphqlHTTP;
+
+const restaurantService = new RestaurantService();
+
+const RestaurantType = new GraphQLObjectType({
+  name: 'Restaurant',
+  fields: {
+    userId: { type: new GraphQLNonNull(GraphQLInt) },
+    restaurantId: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    deliveryTime: { type: new GraphQLNonNull(GraphQLInt) },
+  }
+});
+
+const rootQueryType = new GraphQLObjectType({
+  name: 'RootQuery',
+  description: 'Root of all query types',
+  fields: {
+    restaurants: {
+      type: new GraphQLList(RestaurantType),
+      resolve: async () => {
+        return await restaurantService.getAllRestaurants();
+      }
+    }
+  }
+})
+
+const apiSchema = new GraphQLSchema({
+  query: rootQueryType
+})
+
+export const rootEndpoint = '/graphql';
+
+export default apiMiddleware({
+  graphiql: process.env.NODE_ENV! === 'development',
+  schema: apiSchema
+});
