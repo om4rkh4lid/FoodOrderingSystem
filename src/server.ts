a6dbd4425581fe5807f56b57083fba3f6e24a3fb
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-05 20:55:09 +0200

    added dependencies for graphql

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import environment from "dotenv";
 import Express from "express";
 import morgan from "morgan";
+import api, { rootEndpoint } from "./api";
 
 environment.config();
 
@@ -10,6 +11,8 @@ const mode: string = process.env.NODE_ENV!;
 if (mode === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use(rootEndpoint, api);
 
 const host: string = process.env.HOST!
 const port: number = parseInt(process.env.PORT!);
