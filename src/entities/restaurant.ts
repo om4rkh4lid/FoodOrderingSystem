ea3f5e98c17bc434ef0f0c4f80b09979c06ca26b
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-06 19:01:13 +0200

    added getters to restaurant entity

--- src/entities/restaurant.ts
+++ src/entities/restaurant.ts
@@ -1,18 +1,37 @@
 class Restaurant {
-  private userId: number;
-  private restaurantId: number;
-  private photo?: any;
-  private category?: any;
-  private name: string;
-  private deliveryTime: number;
+  private _userId: number;
+  private _restaurantId: number;
+  private _photo?: any;
+  private _category?: any;
+  private _name: string;
+  private _deliveryTime: number;
 
   constructor(userId: number, restaurantId: number, name: string, deliveryTime: number, photo?: any, category?: any) {
-    this.userId = userId;
-    this.restaurantId = restaurantId;
-    this.name = name;
-    this.deliveryTime = deliveryTime;
-    this.photo = photo;
-    this.category = category;
+    this._userId = userId;
+    this._restaurantId = restaurantId;
+    this._name = name;
+    this._deliveryTime = deliveryTime;
+    this._photo = photo;
+    this._category = category;
+  }
+
+  public get userId() {
+    return this._userId
+  }
+  public get restaurantId() {
+    return this._restaurantId;
+  }
+  public get photo() {
+    return this._photo;
+  }
+  public get category() {
+    return this._category;
+  }
+  public get name() {
+    return this._name;
+  }
+  public get deliveryTime() {
+    return this._deliveryTime;
   }
 
 }
