bd43e29a38dba734b2fb95875944e6265f765ecb
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-14 20:26:22 +0200

    used native postgres instead of dummy data in restaurant service

--- src/services/restaurant.ts
+++ src/services/restaurant.ts
@@ -1,7 +1,19 @@
+import { Client } from "pg";
 import { restaurants } from "../data/restaurants"
+import Restaurant from "../entities/restaurant";
 
 class RestaurantService {
+  private repository;
+
+  constructor(repository: Client) {
+    this.repository = repository;
+  }
+
   getAllRestaurants = async () => {
+    await this.repository.connect();
+    const result = await this.repository.query('SELECT * FROM restaurants;');
+    const restaurants: Restaurant[] = result.rows.map(row => new Restaurant(row.user_id, row.restaurant_id, row.name, row.delivery_time));
+    await this.repository.end();
     return restaurants;
   }
   findRestaurantById = async (restaurantId: number) => {
