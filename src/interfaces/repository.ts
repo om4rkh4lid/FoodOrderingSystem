5afced3c6c4c2cb2365fbea0f8315ad786e212db
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-25 01:48:10 +0200

    created a repository interface

---
+++ src/interfaces/repository.ts
@@ -0,0 +1,6 @@
+interface Repository<T> {
+  create(entity: T): Promise<T>;
+  findById(id: any): Promise<T | null>;
+  update(id: any, updates: any): Promise<T | null>;
+  delete(id: any): Promise<void>;
+}
