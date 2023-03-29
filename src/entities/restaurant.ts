ba92bf331284eda01339a2c0c97d526dd8c29ed8
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-29 22:24:24 +0200

    changed the type of the photo property

--- src/entities/restaurant.ts
+++ src/entities/restaurant.ts
@@ -1,24 +1,24 @@
 class Restaurant {
   private _restaurantId: number;
-  private _photo: Buffer;
+  private _photoUrl: string;
   private _categories: string[];
   private _name: string;
   private _deliveryTime: number;
 
 
-  constructor(restaurantId: number, name: string, deliveryTime: number, categories: string[], photo: Buffer) {
+  constructor(restaurantId: number, name: string, deliveryTime: number, categories: string[], photoUrl: string) {
     this._restaurantId = restaurantId;
     this._name = name;
     this._deliveryTime = deliveryTime;
-    this._photo = photo;
+    this._photoUrl = photoUrl;
     this._categories = categories;
   }
 
   public get restaurantId() {
     return this._restaurantId;
   }
-  public get photo() {
-    return this._photo;
+  public get photoUrl() {
+    return this._photoUrl;
   }
   public get categories() {
     return this._categories;
